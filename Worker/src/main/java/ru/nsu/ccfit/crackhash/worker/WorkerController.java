fd024183fa80d2e0d45a9dd1985d278a496a287f
ZdarovaMujiki <vvremezovv@mail.ru>
2023-04-27 02:59:28 +0700

    multiple tasks

--- Worker/src/main/java/ru/nsu/ccfit/crackhash/worker/WorkerController.java
+++ Worker/src/main/java/ru/nsu/ccfit/crackhash/worker/WorkerController.java
@@ -1,11 +1,15 @@
 package ru.nsu.ccfit.crackhash.worker;
 
 import generated.Request;
+import generated.Response;
 import org.apache.commons.codec.digest.DigestUtils;
 import org.paukov.combinatorics.ICombinatoricsVector;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 import static org.paukov.combinatorics.CombinatoricsFactory.createPermutationWithRepetitionGenerator;
 import static org.paukov.combinatorics.CombinatoricsFactory.createVector;
@@ -14,9 +18,10 @@ import static org.paukov.combinatorics.CombinatoricsFactory.createVector;
 public class WorkerController {
     private final String[] alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
     private final ICombinatoricsVector<String> vector = createVector(alphabet);
+    private final WebClient client = WebClient.create(System.getenv("MANAGER_URL"));
 
     @PostMapping("/internal/api/worker/hash/crack/task")
-    public String task(@RequestBody Request request) {
+    public void task(@RequestBody Request request) {
         int partCount = request.getPartCount();
         int partNumber = request.getPartNumber();
         String targetHash = request.getHash();
@@ -37,6 +42,9 @@ public class WorkerController {
             end += wordsRemain;
         }
 
+        Response response = new Response();
+        response.setId(request.getId());
+
         var generator = createPermutationWithRepetitionGenerator(vector, length);
         long index = 0;
         for (var letters : generator) {
@@ -44,13 +52,22 @@ public class WorkerController {
                 String word = String.join("", letters.getVector());
                 String hash = DigestUtils.md5Hex(word);
 
-                if (hash.equals(targetHash)) return word;
+                if (hash.equals(targetHash)) {
+                    response.setData(word);
+                    break;
+                }
             }
 
             index++;
             if (index == end) break;
         }
 
-        return null;
+        client.post()
+                .uri("/internal/api/manager/hash/crack/request")
+                .contentType(MediaType.TEXT_XML)
+                .body(Mono.just(response), Response.class)
+                .retrieve()
+                .bodyToFlux(String.class)
+                .subscribe();
     }
 }
